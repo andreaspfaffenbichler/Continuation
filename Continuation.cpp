461cc196ece23bbf04480a21967431f46eef1ea3
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2022-03-16 18:13:39 +0100

    add test case sync, fails

--- Continuation.cpp
+++ Continuation.cpp
@@ -26,10 +26,12 @@ namespace
 	{
 		struct promise_type
 		{
+			promise_type() noexcept {}
 			Continuation< R > get_return_object() 
 			{
 				return Continuation< R >{ std_coroutine::coroutine_handle< promise_type >::from_promise( *this ) };
 			}
+
 			struct ResumeContinuation
 			{
 				ResumeContinuation() noexcept {}
@@ -42,7 +44,6 @@ namespace
 				}
 				void await_resume() noexcept {}
 			};
-			promise_type() noexcept {}
 			auto initial_suspend() noexcept { return std_coroutine::suspend_never{}; }
 			auto final_suspend() noexcept { return ResumeContinuation{}; }
 			void return_value( R result ) { result_ = result; }
@@ -59,10 +60,18 @@ namespace
 		explicit Continuation( std_coroutine::coroutine_handle< promise_type > coroutine ) : coroutine_( coroutine ) {}
 
 		bool await_ready() const noexcept{ return false; }
-		void await_suspend( std_coroutine::coroutine_handle<> callingCoroutine ) noexcept { coroutine_.promise().callingCoroutine_ = callingCoroutine; }
+		void await_suspend( std_coroutine::coroutine_handle<> callingCoroutine ) noexcept 
+		{ 
+			coroutine_.promise().callingCoroutine_ = callingCoroutine; 
+		}
 		auto await_resume() { return coroutine_.promise().result_; }
 
-	private:
+		void Rundown() 
+		{
+			while( !coroutine_.done() )
+				coroutine_.resume();
+		}
+
 		std_coroutine::coroutine_handle< promise_type > coroutine_;
 	};
 
@@ -99,61 +108,136 @@ namespace
 #define BOOST_TEST_MESSAGE( x ) \
 	std::cout << x << std::endl;
 
-	std::thread t;
-	bool continuationsRun = false;
 
-	void api( const std::function< void( int ) >& callback )
+	namespace Test
 	{
-		t = std::thread( [ = ] 
-		{ 
-			std::this_thread::sleep_for( std::chrono::seconds{ 5 } );
+		std::thread t;
+		bool continuationsRun = false;
+
+		void apiAsync( const std::function< void( int ) >& callback )
+		{
+			t = std::thread( [ = ] 
+			{ 
+				std::this_thread::sleep_for( std::chrono::seconds{ 5 } );
+				callback( 41 );
+			});
+		}
+
+		void apiSync( const std::function< void( int ) >& callback )
+		{
 			callback( 41 );
-		});
+		}
+
+		Continuation< int > Test1( Api< int > api )
+		{
+			int initalValue = 1;
+ 			BOOST_TEST_MESSAGE( "Start Test1" );
+			int x = co_await CallbackContinuation< int >( api );
+			x += initalValue;
+			BOOST_TEST( x == 42 );
+			BOOST_TEST_MESSAGE( "Test1" );
+			co_return x += 1;
+		}
+
+		Continuation< double > Test2( Api< int > api )
+		{
+			BOOST_TEST_MESSAGE( "Start Test2" );
+			auto x = co_await Test1( api );
+			BOOST_TEST( x == 43 );
+			BOOST_TEST_MESSAGE( "Test2" );
+			co_return x + 1.0;
+		}
+
+		Continuation< double > Test3( Api< int > api )
+		{
+			BOOST_TEST_MESSAGE( "Start Test3" );
+			auto x = co_await Test2( api );
+			BOOST_TEST( x == 44.0 );
+			BOOST_TEST_MESSAGE( "Test3" );
+			co_return x + 1.0;
+		}
+
+		Continuation<> Test4( Api< int > api )
+		{
+			BOOST_TEST_MESSAGE( "Start Test4" );
+			auto x = co_await Test3( api );
+			BOOST_TEST( x == 45.0 );
+			BOOST_TEST_MESSAGE( "Test4" );
+			continuationsRun = true;
+			co_return {};
+		}
 	}
 
-	Continuation< int > Test1X()
-	{
-		int initalValue = 1;
- 		BOOST_TEST_MESSAGE( "Start Test1X" );
-		int x = co_await CallbackContinuation< int >( &api );
-		x += initalValue;
-		BOOST_TEST( x == 42 );
-		BOOST_TEST_MESSAGE( "Test1X" );
-		co_return x += 1;
-	}
-
-	Continuation< double > Test1aX()
-	{
-		BOOST_TEST_MESSAGE( "Start Test1aX" );
-		int x = co_await Test1X();
-		BOOST_TEST( x == 43 );
-		BOOST_TEST_MESSAGE( "Test1aX" );
-		co_return x + 1.0;
-	}
-
-	Continuation<> Test2X()
-	{
-		BOOST_TEST_MESSAGE( "Start Test2X" );
-		auto x = co_await Test1aX();
-		BOOST_TEST( x == 44.0 );
-		BOOST_TEST_MESSAGE( "Test2X" );
-		continuationsRun = true;
-		co_return {};
-	}
+	template< typename R >
+		R SyncronizeHack( auto x )
+		{
+			auto coroutine = x();
+			coroutine.Rundown();
+			return coroutine.coroutine_.promise().result_;
+		}
 }
 
 int main()
 {
 	BOOST_TEST_MESSAGE( "main start" );
+
+	//BOOST_TEST_MESSAGE( "synchron start simple" );
+	//{
+	//	bool run = false;
+	//	auto coro = [ & ]()->Continuation<>
+	//	{
+	//		auto x = co_await Test::Test3( &Test::apiSync );
+	//		BOOST_TEST( x == 45.0 );
+	//		run = true;
+	//		co_return {};
+	//	}();
+	//	BOOST_TEST( run == true );
+	//}
+	//BOOST_TEST_MESSAGE( "synchron start" );
+	//{
+	//	auto x = SyncronizeHack< double >( &B::Test3 );
+	//	BOOST_TEST( x == 45.0 );
+	//}
+
+	//BOOST_TEST_MESSAGE( "synchron literal start" );
+	//{
+	//	auto x = SyncronizeHack< double >( &C::Test3 );
+	//	BOOST_TEST( x == 45.0 );
+	//}
+	//BOOST_TEST_MESSAGE( "synchron literal start simple" );
+	//{
+	//	bool run = false;
+	//	{
+	//		auto coro = [ & ]()->Continuation<>
+	//		{
+	//			auto x = co_await Test::Test3( &Test::apiAsync );
+	//			BOOST_TEST( x == 45.0 );
+	//			run = true;
+	//			co_return {};
+	//		}();
+	//	}
+	//	BOOST_TEST( run == true );
+	//}
+
+	BOOST_TEST_MESSAGE( "synchron start" );
 	{
-		Test2X();
+		Test::continuationsRun = false;
+		Test::Test4( &Test::apiSync );
+		BOOST_TEST( Test::continuationsRun );
 	}
-	BOOST_TEST( !continuationsRun );
-	BOOST_TEST_MESSAGE( "main after Test2X" );
+
+	BOOST_TEST_MESSAGE( "asynchron start" );
+	{
+		Test::continuationsRun = false;
+		Test::Test4( &Test::apiAsync );
+		BOOST_TEST( !Test::continuationsRun );
+	}
+	BOOST_TEST( !Test::continuationsRun );
+	BOOST_TEST_MESSAGE( "main after Test4" );
 	std::this_thread::sleep_for( std::chrono::seconds{ 1 } );
 	BOOST_TEST_MESSAGE( "main after sleep" );
-	t.join();
-	BOOST_TEST( continuationsRun );
+	Test::t.join();
+	BOOST_TEST( Test::continuationsRun );
 	BOOST_TEST_MESSAGE( "after join" );
 }
 
